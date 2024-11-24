e914c0fa75a76d3e9366f5671b5ae6384600a84a
Carlos Daniel <carlosporrasayala@gmail.com>
2024-11-24 05:48:51 -0500

    Agregar navegación entre vistas

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
 export default function Header() {
+  const isActive = ({ isActive }: { isActive: boolean }) =>
+    isActive
+      ? 'text-orange-500 uppercase font-bold'
+      : 'text-white uppercase font-bold';
+
   return (
     <header className='bg-slate-800'>
       <div className='mx-auto container px-5 py-16'>
@@ -6,7 +13,14 @@ export default function Header() {
           <div>
             <img className='w-32' src='/logo.svg' alt='logotipo' />
           </div>
-          <nav></nav>
+          <nav className='flex gap-4'>
+            <NavLink to='/' className={isActive}>
+              Inicio
+            </NavLink>
+            <NavLink to='/favorites' className={isActive}>
+              Favoritos
+            </NavLink>
+          </nav>
         </div>
       </div>
     </header>
