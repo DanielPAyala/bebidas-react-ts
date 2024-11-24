b58b37791751a1c849916612d426e8404badd8b9
Carlos Daniel <carlosporrasayala@gmail.com>
2024-11-24 06:05:08 -0500

    Diseñar el formulario

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { NavLink } from 'react-router-dom';
+import { useMemo } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
 
 export default function Header() {
+  const { pathname } = useLocation();
+
+  const isHome = useMemo(() => pathname === '/', [pathname]);
+
   const isActive = ({ isActive }: { isActive: boolean }) =>
     isActive
       ? 'text-orange-500 uppercase font-bold'
@@ -22,6 +27,46 @@ export default function Header() {
             </NavLink>
           </nav>
         </div>
+
+        {isHome && (
+          <form className='md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6'>
+            <div className='space-y-4'>
+              <label
+                htmlFor='ingredient'
+                className='text-white uppercase font-extrabold text-lg'
+              >
+                Nombre o ingredientes
+              </label>
+              <input
+                id='ingredient'
+                type='text'
+                name='ingredient'
+                className='p-3 w-full rounded-lg focus:outline-none'
+                placeholder='Nombre o Ingrediente. Ej. Vodka, Tequila, Café'
+              />
+            </div>
+            <div className='space-y-4'>
+              <label
+                htmlFor='category'
+                className='text-white uppercase font-extrabold text-lg'
+              >
+                Categoría
+              </label>
+              <select
+                id='category'
+                name='category'
+                className='p-3 w-full rounded-lg focus:outline-none'
+              >
+                <option value=''>-- Seleccione --</option>
+              </select>
+            </div>
+            <input
+              type='submit'
+              value='Buscar Recetas'
+              className='cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase'
+            />
+          </form>
+        )}
       </div>
     </header>
   );
